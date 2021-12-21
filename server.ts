39c0a758424f53ef2d629deeed1d76840947d7e7
keshav <kdooleea@yahoo.ca>
2021-12-21 13:13:17 -0500

    added access control header

--- server.ts
+++ server.ts
@@ -21,6 +21,11 @@ app.use(
     methods: ["GET", "POST"],
   })
 );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json({ limit: "10mb" }));
 app.use(session(sessionOptions));
 
