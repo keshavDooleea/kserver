cde2aca747efd17951f95c60e9c262d3e7333374
keshav <kdooleea@yahoo.ca>
2021-12-20 21:07:37 -0500

    modified cors configs

--- middlewares/headers.middleware.ts
+++ middlewares/headers.middleware.ts
@@ -1,22 +1,24 @@
 import { NextFunction, Request, Response } from "express";
 import cors from "cors";
+import { HTTP_CODE } from "../lib/http-code.lib";
 
-const origins = ["http://localhost:3000", "https://kdserver.vercel.app"];
+// const origins = ["http://localhost:3000", "https://kdserver.vercel.app"];
 
 export const corsOptions: cors.CorsOptions = {
-  origin: origins,
+  origin: "*",
   credentials: true,
-  allowedHeaders: ["Content-Type"],
+  optionsSuccessStatus: HTTP_CODE.OK,
+  methods: "GET, POST",
 };
 
 export const headersMiddleware = (request: Request, response: Response, next: NextFunction) => {
-  const { origin } = request.headers;
+  //   const { origin } = request.headers;
 
-  // only allow required domains
-  if (!origins.includes(origin)) return;
+  //   // only allow required domains
+  //   if (!origins.includes(origin)) return;
 
-  response.header("Access-Control-Allow-Origin", origin);
-  response.header("Access-Control-Allow-Methods", "GET, POST");
-  response.header("Access-Control-Allow-Headers", "Content-Type");
+  //   response.header("Access-Control-Allow-Origin", origin);
+  //   response.header("Access-Control-Allow-Methods", "GET, POST");
+  //   response.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 };
