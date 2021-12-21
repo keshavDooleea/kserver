98c791ca55fab118e684d6b452f5b9c574088d2e
keshav <kdooleea@yahoo.ca>
2021-12-20 19:20:05 -0500

    added cors options

--- middlewares/headers.middleware.ts
+++
@@ -1,7 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export const headersMiddleware = (request: Request, response: Response, next: NextFunction) => {
-  const origins = ["http://localhost:3000"];
-  response.setHeader("Access-Control-Allow-Origin", origins);
-  next();
-};
--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ import { requestLoggerMiddleware } from "./middlewares/request-logger.middleware
 import { errorNotFoundMiddleware } from "./middlewares/error.middleware";
 import { ApiController } from "./controllers/api.controller";
 import { sessionOptions } from "./lib/express-session.lib";
-import { headersMiddleware } from "./middlewares/headers.middleware";
+import { corsOptions } from "./lib/cors.lib";
 
 dotenv.config();
 
@@ -16,12 +16,11 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // middleware configurations
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "10mb" }));
 app.use(session(sessionOptions));
 
 // api
-app.use(headersMiddleware);
 app.use(requestLoggerMiddleware);
 app.use("/", new BaseController().getRouter()); // base endpoint: /
 app.use("/api", new ApiController().getRouter()); // custom endpoints: /api/*
