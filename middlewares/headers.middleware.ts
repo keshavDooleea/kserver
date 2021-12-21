d0feaff2b49cbbba222860a0b123e41db0d4c59f
keshav <kdooleea@yahoo.ca>
2021-12-20 20:51:20 -0500

    checking if origin is allowed

--- middlewares/headers.middleware.ts
+++ middlewares/headers.middleware.ts
@@ -10,7 +10,12 @@ export const corsOptions: cors.CorsOptions = {
 };
 
 export const headersMiddleware = (request: Request, response: Response, next: NextFunction) => {
-  response.header("Access-Control-Allow-Origin", origins);
+  const { origin } = request.headers;
+
+  // only allow required domains
+  if (!origins.includes(origin)) return;
+
+  response.header("Access-Control-Allow-Origin", origin);
   response.header("Access-Control-Allow-Methods", "GET, POST");
   response.header("Access-Control-Allow-Headers", "Content-Type");
   next();
