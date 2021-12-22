f9483af08dca9932c5ef004562f5763c3d083e46
keshav <kdooleea@yahoo.ca>
2021-12-21 20:19:31 -0500

    added expose header in app

--- server.ts
+++ server.ts
@@ -20,6 +20,7 @@ app.use(cors(corsOptions));
 app.use(express.json({ limit: "10mb" }));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Expose-Headers", "Content-Length, X-JSON");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
