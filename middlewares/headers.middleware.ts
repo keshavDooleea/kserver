eb1c68e4ac32fe1b185ca6c1beefed7b93319b22
keshav <kdooleea@yahoo.ca>
2021-12-22 19:44:55 -0500

    cleaned headers middleware

--- middlewares/headers.middleware.ts
+++ middlewares/headers.middleware.ts
@@ -2,10 +2,10 @@ import { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { HTTP_CODE } from "../lib/http-code.lib";
 
-// const origins = ["http://localhost:3000", "https://kdserver.vercel.app"];
+const origins = ["http://localhost:3000", "https://kdserver.vercel.app"];
 
 export const corsOptions: cors.CorsOptions = {
-  origin: "*",
+  origin: origins,
   credentials: true,
   optionsSuccessStatus: HTTP_CODE.OK,
   methods: ["GET", "POST"],
@@ -13,26 +13,19 @@ export const corsOptions: cors.CorsOptions = {
 };
 
 export const headersMiddleware = (request: Request, response: Response, next: NextFunction) => {
-  //   const { origin } = request.headers;
+  const { origin } = request.headers;
 
   //   // only allow required domains
-  //   if (!origins.includes(origin)) return;
+  if (!origins.includes(origin)) return;
 
-  //   response.header("Access-Control-Allow-Origin", origin);
-  //   response.header("Access-Control-Allow-Methods", "GET, POST");
-  //   response.header("Access-Control-Allow-Headers", "Content-Type");
+  response.header("Access-Control-Allow-Origin", origin);
+  response.header("Access-Control-Allow-Methods", "GET, POST");
+  response.header("Access-Control-Allow-Headers", "Content-Type, Origin");
+  response.header("Access-Control-Allow-Credentials", "true");
 
   if (request.method === "OPTIONS") {
-    return response.status(200).send("ok");
+    return response.status(HTTP_CODE.OK).send("ok");
   }
 
-  // handle incoming request as usual
-  console.log("nextin");
   next();
 };
-
-// {
-//     res.header("Access-Control-Expose-Headers", "Content-Length, X-JSON");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   }
